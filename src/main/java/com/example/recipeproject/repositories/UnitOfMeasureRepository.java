67f95c9e165e8f1f69ada07fab2353070a48bc7e
Bruno Ricardo Correa <bruno.correa@sensedia.com>
2022-08-10 14:52:52 -0300

    Creating Integration Test to UnitOfMeasureRepository.

--- src/main/java/com/example/recipeproject/repositories/UnitOfMeasureRepository.java
+++ src/main/java/com/example/recipeproject/repositories/UnitOfMeasureRepository.java
@@ -2,6 +2,7 @@ package com.example.recipeproject.repositories;
 
 import com.example.recipeproject.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
